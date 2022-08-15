29d3656c36a50f15f62f8a34e39b9dc5af69e875
Sergey Miranovich <belarus1552@outlook.com>
2022-08-15 15:10:49 +0300

    added: roles module

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+export class CreateUserDto{
+    login: string;
+    password: string;
+    firstName: string;
+    secondName: string;
+    lastName: string;
+    phone: string;
+    position: string;
+}
